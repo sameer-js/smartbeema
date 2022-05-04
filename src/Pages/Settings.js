388e2eb0e5309316936a737ed813144e5bccc25e
Sameer Jung Subedi <meersj@gmail.commeersj@gmail.comgit config --global user.email meersj@gmail.com>
2022-05-04 14:44:30 +0545

    added admin credentials

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Whitelist = () => {
+  const [allowed, setAllowed] = useState(false);
+  const [email, setEmail] = useState(true);
+  const [password, setPassword] = useState(true);
+
+  const navigate = useNavigate();
+
+  const emailChange = (e) => {
+    if (e.target.value === "smartbeema@gmail.com") {
+      setEmail(false);
+      if (!password) setAllowed(true);
+    }
+  };
+
+  const passwordChange = (e) => {
+    if (e.target.value === "12345") {
+      setPassword(false);
+      if (!email) setAllowed(true);
+    }
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (allowed) navigate("/whitelist");
+  };
+
   return (
     <Form className="loginform">
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>
           <h5>Email address</h5>
         </Form.Label>
-        <Form.Control required type="email" placeholder="Enter email" />
+        <Form.Control
+          onChange={emailChange}
+          required
+          type="email"
+          placeholder="Enter email"
+        />
         <Form.Text className="text-muted">
           Do not share whitelist email with anyone else.
         </Form.Text>
@@ -18,14 +49,23 @@ const Whitelist = () => {
         <Form.Label>
           <h5>Password</h5>
         </Form.Label>
-        <Form.Control required type="password" placeholder="Password" />
+        <Form.Control
+          onChange={passwordChange}
+          required
+          type="password"
+          placeholder="Password"
+        />
       </Form.Group>
 
-      <Link to="/whitelist">
-        <Button className="formbutton" variant="primary" type="submit">
-          Submit
-        </Button>
-      </Link>
+      <Button
+        disabled={!allowed}
+        onClick={submit}
+        className="formbutton"
+        variant="primary"
+        type="submit"
+      >
+        Submit
+      </Button>
     </Form>
   );
 };
